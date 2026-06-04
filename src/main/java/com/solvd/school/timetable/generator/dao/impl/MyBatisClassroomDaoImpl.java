0dcc8e017c0284c94cb0594ec10f3de53ce8a956
Giga <gigamikasha@gemail.com>
2026-06-04 22:50:56 +0400

    add dao implementations

--- src/main/java/com/solvd/school/timetable/generator/dao/impl/MyBatisClassroomDaoImpl.java
+++ src/main/java/com/solvd/school/timetable/generator/dao/impl/MyBatisClassroomDaoImpl.java
@@ -1,5 +1,27 @@
 package com.solvd.school.timetable.generator.dao.impl;
 
-public class MyBatisClassroomDaoImpl {
+import com.solvd.school.timetable.generator.util.MyBatisSessionHolder;
+import com.solvd.school.timetable.generator.dao.ClassroomDao;
+import com.solvd.school.timetable.generator.model.Classroom;
+import org.apache.ibatis.session.SqlSession;
 
+import java.util.List;
+
+public class MyBatisClassroomDaoImpl implements ClassroomDao {
+
+    @Override
+    public List<Classroom> findAll() {
+        try (SqlSession session = MyBatisSessionHolder.openSession()) {
+            ClassroomDao mapper = session.getMapper(ClassroomDao.class);
+            return mapper.findAll();
+        }
+    }
+
+    @Override
+    public Classroom findById(Long id) {
+        try (SqlSession session = MyBatisSessionHolder.openSession()) {
+            ClassroomDao mapper = session.getMapper(ClassroomDao.class);
+            return mapper.findById(id);
+        }
+    }
 }
--- src/main/java/com/solvd/school/timetable/generator/dao/impl/MyBatisSubjectDaoImpl.java
+++ src/main/java/com/solvd/school/timetable/generator/dao/impl/MyBatisSubjectDaoImpl.java
@@ -1,5 +1,27 @@
 package com.solvd.school.timetable.generator.dao.impl;
 
-public class MyBatisSubjectDaoImpl {
+import com.solvd.school.timetable.generator.util.MyBatisSessionHolder;
+import com.solvd.school.timetable.generator.dao.SubjectDao;
+import com.solvd.school.timetable.generator.model.Subject;
+import org.apache.ibatis.session.SqlSession;
 
+import java.util.List;
+
+public class MyBatisSubjectDaoImpl implements SubjectDao {
+
+    @Override
+    public List<Subject> findAll() {
+        try (SqlSession session = MyBatisSessionHolder.openSession()) {
+            SubjectDao mapper = session.getMapper(SubjectDao.class);
+            return mapper.findAll();
+        }
+    }
+
+    @Override
+    public Subject findById(Long id) {
+        try (SqlSession session = MyBatisSessionHolder.openSession()) {
+            SubjectDao mapper = session.getMapper(SubjectDao.class);
+            return mapper.findById(id);
+        }
+    }
 }
